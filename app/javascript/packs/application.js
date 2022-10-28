016b19830252ecf7bc471a5d482166b90ca440c7
dsang501 <80367441+dsang501@users.noreply.github.com>
2022-10-28 10:33:09 +0900

    レイアウト修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,14 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
+  document.addEventListener("turbolinks:load", function() {
+    $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
